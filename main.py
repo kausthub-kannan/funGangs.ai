b6587bfcf24e4f8dda3b5b5c2e2fde60d3eb024c
Kausthub Kannan <kausthubkannan961@gmail.com>
2023-10-12 19:29:31 +0000

    feat: added api service using fastapi

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import torch
 from stylegan import generate_image
-import gradio as gr
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
-noise = torch.randn(36, 256).to("cpu")
+
+app = FastAPI()
 
 
-demo = gr.Interface(
-    generate_image(noise),
-    gr.Image(type="pil"),
-    "image",
-)
+@app.get("/")
+def read_root():
+    return "Welcome to FunGang generator! Generate humorous pixel arts of your gangs!"
 
-if __name__ == "__main__":
-    demo.launch()
+
+@app.get("/generate/{number_of_image}")
+def image_generation(number_of_image: int):
+    try:
+        noise = torch.randn(number_of_image, 256).to("cpu")
+        status, message = generate_image(noise)
+
+        if status == 200:
+            return FileResponse("generated_images/predicted.png")
+        else:
+            return message
+    except RuntimeError:
+        return "Device Memory Overload: Please provide less number of images"
